a4ff417127a4c903bf42cda14b3869fe75dee586
shivammalik <shivammalik1597@gmail.com>
2017-07-05 11:23:37 +0530

    Add files via upload

---
+++ Database/Database1_mysql_create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `Database` (
+	`GIG NO.` INT NOT NULL,
+	`Reference/Part no.` varchar NOT NULL,
+	`Description` TEXT NOT NULL,
+	`Serial no. of tester/panel meter/ gauge` varchar NOT NULL,
+	`Cal Agency` TEXT NOT NULL,
+	`CAL DUE ` varchar NOT NULL
+);
+
